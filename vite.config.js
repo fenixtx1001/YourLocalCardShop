c47212b18b56a630612fa5f741d875331abdb721
fenixtx1001 <johntpickworth@gmail.com>
2025-05-29 15:40:57 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  root: '.', // optional if it's already at root
-  build: {
-    rollupOptions: {
-      input: 'index.html'
-    }
-  }
 })
