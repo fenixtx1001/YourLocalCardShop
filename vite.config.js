1b41dcc19968df6740d0b2a76548e9093bb94076
fenixtx1001 <johntpickworth@gmail.com>
2025-05-29 15:29:43 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-});
+  root: '.', // optional if it's already at root
+  build: {
+    rollupOptions: {
+      input: 'index.html'
+    }
+  }
+})
