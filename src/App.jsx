9d120056aa2069d6a4c6f40fb5f98efea4f93b5b
fenixtx1001 <johntpickworth@gmail.com>
2025-06-02 10:47:19 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Store from "./pages/Store";
-import PackOpener from "./pages/PackOpener";
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Store from './pages/Store';
+import PackOpener from './pages/PackOpener';
 
-function App() {
+export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/store" element={<Store />} />
-        <Route path="/packopener" element={<PackOpener />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/store" element={<Store />} />
+      <Route path="/packopener" element={<PackOpener />} />
+    </Routes>
   );
 }
-
-export default App;
