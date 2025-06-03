d71d3bdc6fbe53ba81cf9853ba3d5e52f88c2fdf
fenixtx1001 <johntpickworth@gmail.com>
2025-06-03 08:23:53 -0500

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import Button from '../components/ui/freshbutton';
+import { Button } from '../components/ui/freshbutton';
 import Inventory from '../components/Inventory';
 import Bank from '../components/Bank';
 
