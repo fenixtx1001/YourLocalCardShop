1a7906defc52113e5d155d833e68b36d07414fe5
fenixtx1001 <johntpickworth@gmail.com>
2025-06-05 14:18:09 -0500

    Update freshbutton.jsx

--- src/components/ui/freshbutton.jsx
+++ src/components/ui/freshbutton.jsx
@@ -1,7 +1,10 @@
-export function Button(props) {
+export function Button({ children, ...props }) {
   return (
-    <button className="px-4 py-2 bg-blue-600 hover:bg-blue-700 text-white rounded shadow">
-      {props.children}
+       <button
+      className="px-4 py-2 bg-blue-600 hover:bg-blue-700 text-white rounded shadow"
+      {...props}
+    >
+      {children}
     </button>
   );
 }
